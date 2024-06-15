08bdce400c2daeb43c7dbe9597cafd792003cf74
prayanaputra <108852536+prayanaputra@users.noreply.github.com>
2024-06-15 09:29:46 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -5,6 +5,9 @@ from PIL import Image
 # Load the model
 model = pickle.load(open('breast.sav', 'rb'))
 
+# Define the front end interface
+st.title('Breast Cancer Prediction')
+
 # Set page config
 st.set_page_config(
     page_title="Breast Cancer Prediction",
@@ -41,42 +44,26 @@ st.markdown(
     """
 )
 
-# Layout for input fields
-col1, col2 = st.columns(2)
-
-with col1:
-    clump_thickness = st.number_input('Clump Thickness', min_value=1, max_value=10, value=1)
-    cell_size_uniformity = st.number_input('Cell Size Uniformity', min_value=1, max_value=10, value=1)
-    cell_shape_uniformity = st.number_input('Cell Shape Uniformity', min_value=1, max_value=10, value=1)
-    marginal_adhesion = st.number_input('Marginal Adhesion', min_value=1, max_value=10, value=1)
-    single_epi_cell_size = st.number_input('Single Epithelial Cell Size', min_value=1, max_value=10, value=1)
-
-with col2:
-    bare_nuclei = st.number_input('Bare Nuclei', min_value=1, max_value=10, value=1)
-    bland_chromatin = st.number_input('Bland Chromatin', min_value=1, max_value=10, value=1)
-    normal_nucleoli = st.number_input('Normal Nucleoli', min_value=1, max_value=10, value=1)
-    mitoses = st.number_input('Mitoses', min_value=1, max_value=10, value=1)
+# Get user input
+clump_thickness = st.number_input('Clump Thickness')
+cell_size_uniformity = st.number_input('Cell Size Uniformity')
+cell_shape_uniformity = st.number_input('Cell Shape Uniformity')
+marginal_adhesion = st.number_input('Marginal Adhesion')
+single_epi_cell_size = st.number_input('Single Epithelial Cell Size')
+bare_nuclei = st.number_input('Bare Nuclei')
+bland_chromatin = st.number_input('Bland Chromatin')
+normal_nucleoli = st.number_input('Normal Nucleoli')
+mitoses = st.number_input('Mitoses')
 
 # Make predictions
 if st.button('Predict'):
-    input_data = (
-        clump_thickness, cell_size_uniformity, cell_shape_uniformity, 
-        marginal_adhesion, single_epi_cell_size, bare_nuclei, 
-        bland_chromatin, normal_nucleoli, mitoses
-    )
+    input_data = (clump_thickness, cell_size_uniformity, cell_shape_uniformity, marginal_adhesion, single_epi_cell_size, bare_nuclei, bland_chromatin, normal_nucleoli, mitoses)
     prediction = model.predict([input_data])
 
-    st.markdown(
-        """
-        ### Prediction Result:
-        """
-    )
-
     if prediction[0] == 0:
-        st.success('The tumor is **benign**.')
+        st.success('The tumor is benign')
     else:
-        st.error('The tumor is **malignant**.')
-
+        st.error('The tumor is malignant')
 # Footer
 st.markdown(
     """
