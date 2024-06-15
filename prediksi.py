58056b216ea05a5b666887624cf1f893a1c0743b
prayanaputra <108852536+prayanaputra@users.noreply.github.com>
2024-06-15 09:30:31 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -55,15 +55,15 @@ bland_chromatin = st.number_input('Bland Chromatin')
 normal_nucleoli = st.number_input('Normal Nucleoli')
 mitoses = st.number_input('Mitoses')
 
-# Make predictions
 if st.button('Predict'):
     input_data = (clump_thickness, cell_size_uniformity, cell_shape_uniformity, marginal_adhesion, single_epi_cell_size, bare_nuclei, bland_chromatin, normal_nucleoli, mitoses)
     prediction = model.predict([input_data])
 
     if prediction[0] == 0:
-        st.success('The tumor is benign')
+        st.write('The tumor is benign')
     else:
-        st.error('The tumor is malignant')
+        st.write('The tumor is malignant')
+        
 # Footer
 st.markdown(
     """
